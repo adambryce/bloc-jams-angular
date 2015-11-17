647117cee4cfa34dc32dc556af90a8983751b4c8
Adam Forrester <adam@grapedesignllc.com>
2015-11-17 11:26:32 -0600

    Checkpoint-3-Assignment

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,10 +14,23 @@ myAppModule.config(function($stateProvider, $locationProvider) {
      });
 
     // For this project, need to know how to configure at least four aspects of a state: its name, URL route, controller, and template.
-    $stateProvider.state('album', {
-         url: '/album',
-         controller: 'Album.controller',
-         templateUrl: '/templates/album.html'
-     });
+    $stateProvider
+        .state('landing', {
+             url: '/',
+             controller: 'Landing.controller',
+             templateUrl: '/templates/landing.html'
+         });
+
+        .state('collection', {
+             url: '/collection',
+             controller: 'Collection.controller',
+             templateUrl: '/templates/collection.html'
+         });
+
+        .state('album', {
+             url: '/album',
+             controller: 'Album.controller',
+             templateUrl: '/templates/album.html'
+         });
     
  });
